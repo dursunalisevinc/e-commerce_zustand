2c6f7f542f614f7543c8cfe50ede461c59df4e4c
Dursun Ali Sevinç <dursunalisevinc@Dursun-MacBook-Air.local>
2026-01-30 20:57:47 +0300

    1

--- zustand_example/src/shared/Layout.tsx
+++ zustand_example/src/shared/Layout.tsx
@@ -6,10 +6,25 @@ import Navbar from '../components/Navbar'
 const Layout = () => {
   return (
     <div className='h-screen flex flex-col'>
-      <Navbar/>
-      <div className='grow bg-secondary-100'><Outlet /></div>
+      <Navbar />
+      <div className='px-[1.5rem] py-[1rem] bg-primary-600 py-4'>
+        Kategoriler
+      </div>
+      <div className='grid grid-cols-12 grow bg-secondary-100'>
+        <div className='col-span-3 bg-white p-[1.5rem]'>
+          <div className='h-full flex flex-col gap-4'>
+            <div className='h-[10rem] bg-primary-200'>sdsd</div>
+             <div className='h-[4rem] bg-primary-500'>sdsd</div>
+            <div className='grow overflow-y-auto bg-primary-800' >bgpsd</div>
+            <div className='h-[3rem] bg-primary-300'>sdsd</div>
+          </div>
+        </div>
+        <div className="col-span-9">
+          <Outlet />
+        </div>
+      </div>
     </div>
-    
+
   )
 }
 
