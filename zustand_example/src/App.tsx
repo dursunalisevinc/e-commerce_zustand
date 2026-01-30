50fd4b25fa6890dd1c24ed6d42640e4866a6aff6
Dursun Ali Sevinç <dursunalisevinc@Dursun-MacBook-Air.local>
2026-01-30 12:44:01 +0300

    added to light/dark theme

--- zustand_example/src/App.tsx
+++ zustand_example/src/App.tsx
@@ -1,11 +1,30 @@
 
 function App() {
 
-   return (
-      <div className="bg-red-200">
-         Added to Tailwind
+  const changeTheme = (theme: "light" | "dark") => {
+    document.documentElement.setAttribute("data-theme", theme);
+  };
+
+  return (
+    <div className="h-screen bg-secondary-100 flex  flex-col gap-4 p-4">
+
+      <div className="bg-primary-700">
+        Test
       </div>
-   )
+      <button
+        onClick={() => changeTheme("light")}
+        className="bg-blue-600 text-white px-4 py-2 rounded-md"
+      >
+        beyaz
+      </button>
+      <button
+        onClick={() => changeTheme("dark")}
+        className="bg-blue-600 text-white px-4 py-2 rounded-md"
+      >
+        siyah
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
